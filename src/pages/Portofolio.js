856b8cc6fa7789c0019bd19aad62e250da91b417
Made Rezananda Putra <maderezanandaputra@172-10-33-12.lightspeed.bcvloh.sbcglobal.net>
2022-07-06 07:46:51 +0700

    add label portfolio

--- src/pages/Portofolio.js
+++ src/pages/Portofolio.js
@@ -8,6 +8,10 @@ const Portofolio = () => {
     <>
       <div className='flex flex-col items-center w-full mt-2 gap-4'>
         <div className='flex flex-col items-center justify-center gap-4 md:w-1/2 px-4 py-2 rounded-lg border border-gray-100 dark:border-gray-500'>
+          <div>
+            <p className='font-bold'>-MY PORTFOLIO-</p>
+            <div className='w-full h-3 rounded-full bg-blue-500'></div>
+          </div>
           <ul className='flex items-center bg-blue-50 gap-2 px-1 py-1 rounded-full dark:bg-black'>
             <li>
               <button onClick={() => setTab(1)} className={tab === 1 ? 'flex bg-blue-500 px-2 rounded-full text-white' : 'flex px-2 text-gray-400'}>Business Analyst</button>
